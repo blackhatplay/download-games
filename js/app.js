5384102e2607d4643fc5880ae35ae177a98f9ede
blackhatplay <mannuall4u@gmail.com>
2020-08-05 22:27:52 +0530

    js restructure

--- js/app.js
+++ js/app.js
@@ -8,9 +8,28 @@ const body = document.querySelector("body");
 const svgLoading = `<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="160px" height="20px" viewBox="0 0 128 16" xml:space="preserve"><path fill="#fbdaa3" fill-opacity="0.42" d="M6.4,4.8A3.2,3.2,0,1,1,3.2,8,3.2,3.2,0,0,1,6.4,4.8Zm12.8,0A3.2,3.2,0,1,1,16,8,3.2,3.2,0,0,1,19.2,4.8ZM32,4.8A3.2,3.2,0,1,1,28.8,8,3.2,3.2,0,0,1,32,4.8Zm12.8,0A3.2,3.2,0,1,1,41.6,8,3.2,3.2,0,0,1,44.8,4.8Zm12.8,0A3.2,3.2,0,1,1,54.4,8,3.2,3.2,0,0,1,57.6,4.8Zm12.8,0A3.2,3.2,0,1,1,67.2,8,3.2,3.2,0,0,1,70.4,4.8Zm12.8,0A3.2,3.2,0,1,1,80,8,3.2,3.2,0,0,1,83.2,4.8ZM96,4.8A3.2,3.2,0,1,1,92.8,8,3.2,3.2,0,0,1,96,4.8Zm12.8,0A3.2,3.2,0,1,1,105.6,8,3.2,3.2,0,0,1,108.8,4.8Zm12.8,0A3.2,3.2,0,1,1,118.4,8,3.2,3.2,0,0,1,121.6,4.8Z"/><g><path fill="#f5a623" fill-opacity="1" d="M-42.7,3.84A4.16,4.16,0,0,1-38.54,8a4.16,4.16,0,0,1-4.16,4.16A4.16,4.16,0,0,1-46.86,8,4.16,4.16,0,0,1-42.7,3.84Zm12.8-.64A4.8,4.8,0,0,1-25.1,8a4.8,4.8,0,0,1-4.8,4.8A4.8,4.8,0,0,1-34.7,8,4.8,4.8,0,0,1-29.9,3.2Zm12.8-.64A5.44,5.44,0,0,1-11.66,8a5.44,5.44,0,0,1-5.44,5.44A5.44,5.44,0,0,1-22.54,8,5.44,5.44,0,0,1-17.1,2.56Z"/><animateTransform attributeName="transform" type="translate" values="23 0;36 0;49 0;62 0;74.5 0;87.5 0;100 0;113 0;125.5 0;138.5 0;151.5 0;164.5 0;178 0" calcMode="discrete" dur="1170ms" repeatCount="indefinite"/></g></svg>`;
 const maxPost = 8;
 
+function uiInteraction(e) {
+  if (e === "update") {
+    homeInfo.style.display = "none";
+    homeInfo.parentElement.style.justifyContent = "flex-start";
+    // body.style.height = "fit-content";
+    getdata(searchQuery.value);
+  }
+
+  if (e === "reset") {
+    homeInfo.style.display = "block";
+    homeInfo.parentElement.style.justifyContent = "center";
+    searchList.innerHTML = "";
+  }
+}
+
 function getLink(link) {
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost/api/api.php", true);
+  xhr.open(
+    "POST",
+    "https://testtravelhorse.000webhostapp.com/api/api.php",
+    true
+  );
 
   //Send the proper header information along with the request
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -62,7 +81,7 @@ function getPost(e, link) {
 
 function getdata(s) {
   if (!s == "") {
-    fetch(`http://localhost/api/getpost.php?s=${s}`)
+    fetch(`https://testtravelhorse.000webhostapp.com/api/getpost.php?s=${s}`)
       .then((res) => res.json())
       .catch((err) => console.log(err))
       .then((data) => {
@@ -110,26 +129,18 @@ searchQuery.addEventListener("focusin", () => {});
 
 searchQuery.addEventListener("focusout", () => {
   if (searchQuery.value == "") {
-    // body.style.removeProperty("height");
-    homeInfo.style.display = "block";
-    homeInfo.parentElement.style.justifyContent = "center";
-    searchList.innerHTML = "";
+    uiInteraction("reset");
   }
 });
 
 searchQuery.addEventListener("keyup", (e) => {
   if (e.keyCode == 13) {
-    homeInfo.style.display = "none";
-    homeInfo.parentElement.style.justifyContent = "flex-start";
-    // body.style.height = "fit-content";
-    getdata(searchQuery.value);
+    uiInteraction("update");
   }
 });
 
 searchBtn.addEventListener("click", (e) => {
-  homeInfo.style.display = "none";
-  homeInfo.parentElement.style.justifyContent = "flex-start";
-  getdata(searchQuery.value);
+  uiInteraction("update");
 });
 
 searchList.addEventListener("click", (e) => {
@@ -139,7 +150,11 @@ searchList.addEventListener("click", (e) => {
     selectedItem.innerHTML = svgLoading;
 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost/api/getpost.php", true);
+    xhr.open(
+      "POST",
+      "https://testtravelhorse.000webhostapp.com/api/getpost.php",
+      true
+    );
 
     //Send the proper header information along with the request
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
